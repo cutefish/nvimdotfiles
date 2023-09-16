e29c3c72bbe4815e35e928c0cf98d7ec25308d96
Xiao Yu <xiao.yu@oracle.com>
2023-09-15 21:22:43 -0700

    completed remap.lua

--- lua/xy0/remap.lua
+++ lua/xy0/remap.lua
@@ -1,2 +1,60 @@
+--------------------------------------------------------------------------------
+-- Leader key
+--------------------------------------------------------------------------------
 vim.g.mapleader = ","
+
+--------------------------------------------------------------------------------
+-- Terminal settings
+--------------------------------------------------------------------------------
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.keymap.set("n", "<leader>vt", ":execute ':vsp term://bash'<CR>a")
+vim.keymap.set("n", "<leader>st", ":execute ':sp term://bash'<CR>a")
+vim.keymap.set("n", "<leader>tt", ":execute ':tabnew term://bash'<CR>a")
+-- Exit terminal mode
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+
+--------------------------------------------------------------------------------
+-- Windows navigations
+--------------------------------------------------------------------------------
+vim.keymap.set("t", "<c-h>", "<c-\\><c-N><c-w>h")
+vim.keymap.set("t", "<c-j>", "<c-\\><c-N><c-w>j")
+vim.keymap.set("t", "<c-k>", "<c-\\><c-N><c-w>k")
+vim.keymap.set("t", "<c-l>", "<c-\\><c-N><c-w>l")
+vim.keymap.set("i", "<c-h>", "<c-\\><c-N><c-w>h")
+vim.keymap.set("i", "<c-j>", "<c-\\><c-N><c-w>j")
+vim.keymap.set("i", "<c-k>", "<c-\\><c-N><c-w>k")
+vim.keymap.set("i", "<c-l>", "<c-\\><c-N><c-w>l")
+vim.keymap.set("n", "<c-h>", "<c-w>h")
+vim.keymap.set("n", "<c-j>", "<c-w>j")
+vim.keymap.set("n", "<c-k>", "<c-w>k")
+vim.keymap.set("n", "<c-l>", "<c-w>l")
+vim.keymap.set("t", "<a-h>", "<c-\\><c-N>:tabprev<CR>")
+vim.keymap.set("t", "<a-j>", "<c-\\><c-N>:tabnext<CR>")
+vim.keymap.set("n", "<a-h>", ":tabprev<CR>")
+vim.keymap.set("n", "<a-l>", ":tabnext<CR>")
+
+--------------------------------------------------------------------------------
+-- Tab navigations
+--------------------------------------------------------------------------------
+vim.keymap.set("n", "<leader>tn", "<c-w>T<CR>")
+vim.keymap.set("n", "<leader>tm", ":tabm<Space>")
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>")
+vim.keymap.set("n", "<leader>to", ":tabonly<CR>")
+vim.keymap.set("n", "<leader>tH", ":-tabmove<CR>")
+vim.keymap.set("n", "<leader>tL", ":+tabmove<CR>")
+vim.keymap.set("n", "<leader>t1", "1gt")
+vim.keymap.set("n", "<leader>t2", "2gt")
+vim.keymap.set("n", "<leader>t3", "3gt")
+vim.keymap.set("n", "<leader>t4", "4gt")
+vim.keymap.set("n", "<leader>t5", "5gt")
+vim.keymap.set("n", "<leader>t6", "6gt")
+vim.keymap.set("n", "<leader>t7", "7gt")
+vim.keymap.set("n", "<leader>t8", "8gt")
+vim.keymap.set("n", "<leader>t9", "9gt")
+vim.keymap.set("n", "<leader>t0", "10gt")
+
+--------------------------------------------------------------------------------
+-- Misc
+--------------------------------------------------------------------------------
 vim.keymap.set("n", "nl", "<Insert><cr><esc>")
