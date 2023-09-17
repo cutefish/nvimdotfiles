753b03b5e6393ae672a2a4f5c605ed361ab84c29
Xiao Yu <xiao.yu@oracle.com>
2023-09-17 12:30:30 -0700

    telescope integration with lspconfig and telescope mapping of select all

--- after/plugin/fuzzyfind.lua
+++ after/plugin/fuzzyfind.lua
@@ -1,6 +1,19 @@
+local telescope = require('telescope')
+local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.git_files, {})
 vim.keymap.set('n', '<leader>A', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<c-s>"] = actions.select_all,
+      },
+    },
+  }
+}
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,12 +20,6 @@ lspconfig.pyright.setup {
 
 -- Global mappings.
 local telescope_builtin = require('telescope.builtin')
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -37,6 +31,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
+    vim.keymap.set('n', '<space>e', telescope_builtin.diagnostics, opts)
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
