21757c62744cd22be54e80a636af6ba3df6ac1ca
Xiao Yu <xiao.yu@oracle.com>
2023-09-18 01:04:54 -0700

    update

--- after/plugin/versionctrl.lua
+++ after/plugin/versionctrl.lua
@@ -1,14 +1,24 @@
 --------------------------------------------------------------------------------
 -- Fugitive
 --------------------------------------------------------------------------------
+local function use_default_if_empty(s, d)
+    if s == nil or s == '' then
+        return d
+    else
+        return s
+    end
+end
 vim.api.nvim_create_user_command("Gl", "Git log", {})
-vim.api.nvim_create_user_command("Gst", "Git status", {})
+vim.api.nvim_create_user_command("Gs", "Git status", {})
 vim.api.nvim_create_user_command("Gd", "Git diff", {})
 vim.api.nvim_create_user_command("Gb", "Git blame", {})
 vim.api.nvim_create_user_command("Gup", function(opts)
     vim.cmd("Git add . ")
-    vim.cmd('Git ci -m ' .. (opts.args or "update"))
+    vim.cmd('Git ci -m ' .. (use_default_if_empty(opts.args, "update")))
     vim.cmd('Git push')
+end, { nargs="?" })
+vim.api.nvim_create_user_command("Gv", function(opts)
+    vim.cmd("Gvdiffsplit " .. opts.args)
 end, { nargs="?" })
 
 --------------------------------------------------------------------------------
