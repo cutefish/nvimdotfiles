0cb810a457ba9a342ef6995895d12707e4aca343
Xiao Yu <xiao.yu@oracle.com>
2023-09-17 21:16:29 -0700

    added Diffview

--- after/plugin/versionctrl.lua
+++ after/plugin/versionctrl.lua
@@ -1,12 +1,19 @@
+--------------------------------------------------------------------------------
+-- Fugitive
+--------------------------------------------------------------------------------
 vim.api.nvim_create_user_command("Gl", "Git log", {})
 vim.api.nvim_create_user_command("Gst", "Git status", {})
 vim.api.nvim_create_user_command("Gd", "Git diff", {})
 vim.api.nvim_create_user_command("Gb", "Git blame", {})
-vim.api.nvim_create_user_command("Gup", function(arg)
+vim.api.nvim_create_user_command("Gup", function(opts)
     vim.cmd("Git add . ")
-    vim.cmd('Git ci -m ' .. (arg.args or "update"))
+    vim.cmd('Git ci -m ' .. (opts.args or "update"))
     vim.cmd('Git push')
-end, {})
-vim.api.nvim_create_user_command("Gdv", function(arg)
-    vim.cmd("Gvdiffsplit " .. arg.args) end, {})
+end, { nargs="?" })
 
+--------------------------------------------------------------------------------
+-- Diffview
+--------------------------------------------------------------------------------
+vim.api.nvim_create_user_command("Dv", function(opts)
+    vim.cmd("DiffviewOpen " .. (opts.args or ""))
+end, { nargs="?" })
